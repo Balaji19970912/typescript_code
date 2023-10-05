c316a9d66fd9f7a251c9f9bcb695098005b75f89
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2023-10-05 10:44:06 +0000

    array examples

---
+++ day_01/myArray1.ts
@@ -0,0 +1,22 @@
+// const mCs = ['Luffy','Zoro','Sanji','Shanks','Garp']
+
+const mCs: string[] = []
+// const powerLevel :number[] = []
+const powerLevel : Array<number> = []
+
+type User = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLmodels : number[][] = [
+    [255,255,255],
+    []
+]
+
+mCs.push('Luffy')
+powerLevel.push(100)
+
+allUsers.push({name: 'Zoro', isActive: true})
